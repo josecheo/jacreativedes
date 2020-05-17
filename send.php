aba690a6e1dac483d4341ef484ee48860b7cb9bd
jalvarez <jalvarez@solera.pe>
2020-05-17 15:43:33 -0500

    fixmessa

--- send.php
+++ send.php
@@ -4,20 +4,21 @@ $number = $_POST['number'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-$header = 'From: ' . $email . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+ $header = 'From: ' . $email . " \r\n";
+ $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+ $header .= "Mime-Version: 1.0 \r\n";
+ $header .= "Content-Type: text/plain";
 
 $mensaje = "Este mensaje fue enviado por " . $name . ",\r\n";
-$mensaje .= "Su e-mail es: " . $email . " \r\n";
-$mensaje .= "Mensaje: " . $_POST['message'] . " \r\n";
-$mensaje .= "Enviado el " . date('d/m/Y', time());
+ $mensaje .= "Su e-mail es: " . $email . " \r\n";
+ $mensaje .= "Mensaje: " . $_POST['message'] . " \r\n";
+ $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'contacto@jacreativedes.com';
-$asunto = 'Mensaje de mi sitio web';
+ $para = 'contacto@jacreativedes.com';
+ $asunto = 'Mensaje de mi sitio web';
 
-mail($para, $asunto, utf8_decode($mensaje), $header);
+ mail($para, $asunto, utf8_decode($mensaje), $header);
 
-header("Location:index.html");
+ echo "<scrip>aler('correo enviado')</scrip>";
+ echo "setTimeout(\"location.href='index.html'\",1000)</scrip>";
 ?>
