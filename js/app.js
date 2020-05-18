d020b6bb357a8c85944d3a531934325b1bac601b
jalvarez <jalvarez@solera.pe>
2020-05-17 21:45:45 -0500

    send

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// let form = document.getElementById('form')
+// let xhr = new XMLHttpRequest();
+
+// form.addEventListener('submit',function(e) {
+// e.preventDefault();
+
+// let data = new FormData(form);
+// xhr.open('POST','http://localhost:4000/send-email' );
+// xhr.onload= () => {
+// console.log('conexion')
+
+// }
+
+// })
